63f59f7eed7f8db5002ac6f984e8303cf3d5e9e9
krymtkts <takatoshi.kuriyama@gmail.com>
2025-01-06 22:11:09 +0900

    Add benchmarks for SimpleRecord12 and StructRecord12.

--- src/benchmark/Benchmarks.fs
+++ src/benchmark/Benchmarks.fs
@@ -1,4 +1,4 @@
-module benchmark
+﻿module benchmark
 
 open BenchmarkDotNet.Attributes
 
@@ -7,14 +7,45 @@ type SimpleRecord3 =
       StringValue: string
       BoolValue: bool }
 
+type SimpleRecord12 =
+    { IntValue: int
+      StringValue: string
+      BoolValue: bool
+      IntValue2: int
+      StringValue2: string
+      BoolValue2: bool
+      IntValue3: int
+      StringValue3: string
+      BoolValue3: bool
+      IntValue4: int
+      StringValue4: string
+      BoolValue4: bool }
+
 [<Struct>]
 type StructRecord3 =
     { IntValue: int
       StringValue: string
       BoolValue: bool }
 
+[<Struct>]
+type StructRecord12 =
+    { IntValue: int
+      StringValue: string
+      BoolValue: bool
+      IntValue2: int
+      StringValue2: string
+      BoolValue2: bool
+      IntValue3: int
+      StringValue3: string
+      BoolValue3: bool
+      IntValue4: int
+      StringValue4: string
+      BoolValue4: bool }
+
 let updateSimpleRecord3 (record: SimpleRecord3) (i: int) = { record with IntValue = i }
 let updateStructRecord3 (record: StructRecord3) (i: int) = { record with IntValue = i }
+let updateSimpleRecord12 (record: SimpleRecord12) (i: int) = { record with IntValue = i }
+let updateStructRecord12 (record: StructRecord12) (i: int) = { record with IntValue = i }
 
 [<MemoryDiagnoser>]
 type Benchmarks() =
@@ -37,11 +68,49 @@ type Benchmarks() =
         |> ignore
 
     [<Benchmark>]
-    member __.StructRecord() =
+    member __.StructRecord3() =
         __.data
         |> Seq.fold
             updateStructRecord3
             { IntValue = 0
               StringValue = "Hello"
               BoolValue = true }
+        |> ignore
+
+    [<Benchmark>]
+    member __.SimpleRecord12() =
+        __.data
+        |> Seq.fold
+            updateSimpleRecord12
+            { IntValue = 0
+              StringValue = "Hello"
+              BoolValue = true
+              IntValue2 = 0
+              StringValue2 = "Hello"
+              BoolValue2 = true
+              IntValue3 = 0
+              StringValue3 = "Hello"
+              BoolValue3 = true
+              IntValue4 = 0
+              StringValue4 = "Hello"
+              BoolValue4 = true }
+        |> ignore
+
+    [<Benchmark>]
+    member __.StructRecord12() =
+        __.data
+        |> Seq.fold
+            updateStructRecord12
+            { IntValue = 0
+              StringValue = "Hello"
+              BoolValue = true
+              IntValue2 = 0
+              StringValue2 = "Hello"
+              BoolValue2 = true
+              IntValue3 = 0
+              StringValue3 = "Hello"
+              BoolValue3 = true
+              IntValue4 = 0
+              StringValue4 = "Hello"
+              BoolValue4 = true }
         |> ignore
